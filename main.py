126a4ff5aa8f98dc8aeaa42f87389464203aafc7
wccjcc <goqkfkrltl12@naver.com>
2024-07-22 14:37:15 +0900

    use cv

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("woojoo")
-print("this is RRT algorithm")
-print("RRT_algorithm")
+import numpy as np
+import random
+import math
+import cv2
+
+# 이미지 변환
+img_path = '/Users/jungwoojoo/Desktop/Study/kimiro/Soft_Robotics_Lab/RRT_algo/small_test1_result.png'
+
+img = cv2.imread(img_path)
+cv2.imshow('Image',img)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
